3e0116faf83915d8bd7c0031751e2749d1444235
ksfzhaohui <ksfzhaohui@126.com>
2016-09-12 21:48:49 +0800

    pac动态加载

--- src/main/java/org/netty/config/PacLoader.java
+++ src/main/java/org/netty/config/PacLoader.java
@@ -1,10 +1,13 @@
 package org.netty.config;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -24,8 +27,41 @@ public class PacLoader {
 	private static Logger log = Logger.getLogger(PacLoader.class);
 
 	private static List<String> domainList = new ArrayList<String>();
+	private static List<String> tempList = new ArrayList<String>();
 
-	public static void load(String file) throws Exception {
+	/** 重加载的间隔时间 **/
+	private static int reloadTime = 5;
+
+	private static long lastModify;
+
+	public static void load(final String filePath) throws Exception {
+		File file = new File(filePath);
+		if (!file.exists()) {
+			throw new RuntimeException("file = " + filePath + " is not exist!");
+		}
+		if (file.lastModified() == lastModify) {
+			return;
+		}
+		lastModify = file.lastModified();
+
+		loadFile(filePath);
+
+		Executors.newScheduledThreadPool(1).scheduleWithFixedDelay(
+				new Runnable() {
+
+					@Override
+					public void run() {
+						try {
+							load(filePath);
+						} catch (Exception e) {
+							log.error(e);
+						}
+					}
+				}, reloadTime, reloadTime, TimeUnit.SECONDS);
+	}
+
+	private synchronized static void loadFile(String file) throws Exception {
+		tempList.clear();
 		InputStream in = null;
 		try {
 			DocumentBuilder builder = DocumentBuilderFactory.newInstance()
@@ -36,9 +72,10 @@ public class PacLoader {
 
 			if (list.getLength() > 0) {
 				for (int j = 0; j < list.getLength(); j++) {
-					domainList.add(list.item(j).getTextContent());
+					tempList.add(list.item(j).getTextContent());
 				}
 			}
+			setDomainList(tempList);
 			log.info("load pac !");
 		} catch (Exception e) {
 			throw e;
@@ -52,13 +89,18 @@ public class PacLoader {
 		}
 	}
 
+	private synchronized static void setDomainList(List<String> tempList) {
+		domainList.clear();
+		domainList.addAll(tempList);
+	}
+
 	/**
 	 * 指定的host是否需要代理
 	 * 
 	 * @param host
 	 * @return
 	 */
-	public static boolean isProxy(String host) {
+	public synchronized static boolean isProxy(String host) {
 		for (String domain : domainList) {
 			if (host.contains(domain)) {
 				return true;
